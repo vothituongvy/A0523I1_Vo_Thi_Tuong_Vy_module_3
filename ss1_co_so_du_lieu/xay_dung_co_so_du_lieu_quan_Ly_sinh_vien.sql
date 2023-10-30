e7d0163875eabec5465a73769188ba7971f7eb6e
vothituongvy <Vothituongvyvttv@gmail.com>
2023-10-30 18:30:54 +0700

    nop bai tap 1

--- ss1_co_so_du_lieu/xay_dung_co_so_du_lieu_quan_Ly_sinh_vien.sql
+++ ss1_co_so_du_lieu/xay_dung_co_so_du_lieu_quan_Ly_sinh_vien.sql
@@ -1,22 +1,18 @@
+create database `student_management`;
 use `student_management`;
-CREATE TABLE students (
+CREATE TABLE student (
     id INT PRIMARY KEY,
     `name` VARCHAR(50),
     age INT,
     country VARCHAR(50)
 );
-INSERT INTO `student_management`.`students` (`id`, `name`, `age`, `country`) VALUES ('1', 'Tran van A', '11', 'Ha Noi');
-CREATE TABLE Class (
+CREATE TABLE Classs (
     id INT PRIMARY KEY,
     `name` VARCHAR(50)
 );
-INSERT INTO `student_management`.`class` (`id`, `name`) VALUES ('1', 'A05');
-INSERT INTO `student_management`.`class` (`id`, `name`) VALUES ('2', 'A06');
-CREATE TABLE teacher (
+CREATE TABLE teachers (
     id INT PRIMARY KEY,
     `name` VARCHAR(50),
     age INT,
     country VARCHAR(50)
 );
-INSERT INTO `student_management`.`teacher` (`id`, `name`, `age`, `country`) VALUES ('1', 'Nguyen Van A', '27', 'Quang Nam');
-INSERT INTO `student_management`.`teacher` (`id`, `name`, `age`, `country`) VALUES ('2', 'Nguyen Van B', '29', 'Da Nang');
